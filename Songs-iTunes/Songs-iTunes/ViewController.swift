60834df418e1f439974adf737a2b1dd38747ccb4
Damián González <damgonzalez@gobertia.com>
2021-03-10 11:32:08 +0100

    Just an empty project

---
+++ Songs-iTunes/Songs-iTunes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Songs-iTunes
+//
+//  Created by Damián González Fernández on 10/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
